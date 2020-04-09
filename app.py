5414f8f15a2977930739e3a6e69e49a7df02ea34
iwangara <ikwangara@gmail.com>
2020-04-09 14:35:35 +0300

    sessions and questions api done

--- app.py
+++ app.py
@@ -1,11 +1,79 @@
-from flask import Flask
+from flask import Flask,request, jsonify, make_response
+from flaskext.mysql import MySQL
+import pymysql
+mysql = MySQL()
 
 app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
+mysql.init_app(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+#get live sessions
+@app.route('/sessions/<string:language>')
+def sessions(language):
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM livesessions WHERE  language=%s",language)
+        rows = cursor.fetchall()
+        resp = jsonify(rows)
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+
+@app.route('/questions/<int:id>')
+def questions(id):
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM livesessions WHERE  id=%s",id)
+        livesession = cursor.fetchall()
+        langauge = livesession[0]['language']
+        stype = livesession[0]['type']
+        session =livesession[0]['session']
+        tmp = session.split(",")
+        sess =[]
+        for x in tmp:
+            sess.append(f"{str(x)}")
+        sesions =tuple(sess)#','.join(sess)
+
+
+
+        print(sesions)
+        if stype=='Apollo':
+            cursor.execute(f"""SELECT * FROM apollos WHERE language=%s AND  session1 IN {sesions} """,langauge)
+            exercise =cursor.fetchall()
+            resp = jsonify(exercise)
+
+            resp.status_code = 200
+            return resp
+
+
+
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
 
 
 if __name__ == '__main__':
